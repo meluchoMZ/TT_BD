63f80a18dd724c18efc8405c954b0014169f0fa9
meluchoMZ <62347665+meluchoMZ@users.noreply.github.com>
2020-05-07 11:13:32 +0200

    Also try.
    To remove the tables created until all is properly done.

---
+++ script_borrado.sql
@@ -0,0 +1,11 @@
+/* Script de instalación da base de datos do traballo tutelado BD(2019-2020)
+   Opción 1 : Xestión de pandemias
+   Autor: Miguel Blanco Godón 78809752E 
+   Grupo 2.4
+*/
+
+/* Borrado da táboa "Pacientes" */
+DROP TABLE pacientes;
+
+/* Borrado da táboa "Teléfonos_pacientes" */
+DROP TABLE telefonos_pacientes;
