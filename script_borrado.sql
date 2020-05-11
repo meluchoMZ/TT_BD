b5cb52ea9dd6282fe678bac472734c638bc52558
meluchoMZ <miguelgodon@outlook.es>
2020-05-11 18:57:52 +0200

    11/05/2020 18:57

--- script_borrado.sql
+++ script_borrado.sql
@@ -5,13 +5,13 @@
 */
 
 /* Borrado da táboa "Pacientes" */
-DROP TABLE IF EXISTS pacientes CASCADE CONSTRAINTS PURGE;
+DROP TABLE pacientes CASCADE CONSTRAINTS PURGE;
 
 /* Borrado da táboa "Sanitarios" */
-DROP TABLE IF EXISTS sanitarios CASCADE CONSTRAINTS PURGE;
+DROP TABLE sanitarios CASCADE CONSTRAINTS PURGE;
 
 /* Borrado da táboa "Teléfonos_pacientes" */
-DROP TABLE IF EXISTS telefonos_pacientes CASCADE CONSTRAINTS PURGE;
+DROP TABLE telefonos_pacientes CASCADE CONSTRAINTS PURGE;
 
 /* Borrado da táboa "Teléfonos_sanitarios" */
-DROP TABLE IF EXISTS sanitarios CASCADE CONSTRAINTS PURGE;
+DROP TABLE sanitarios CASCADE CONSTRAINTS PURGE;
