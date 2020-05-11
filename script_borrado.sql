ba224d9906317ed8f80a38bf77fd5c9408973d5e
meluchoMZ <miguelgodon@outlook.es>
2020-05-11 17:59:59 +0200

    11/05/2020 17:59

--- script_borrado.sql
+++ script_borrado.sql
@@ -1,17 +1,17 @@
 /* Script de borrado da base de datos do traballo tutelado BD(2019-2020)
-   Opción 1 : Xestión de pandemias
-   Autor: Miguel Blanco Godón 78809752E 
-   Grupo 2.4
+	Opción 1 : Xestión de pandemias
+	Autor: Miguel Blanco Godón 78809752E 
+	Grupo 2.4
 */
 
 /* Borrado da táboa "Pacientes" */
-DROP TABLE pacientes;
+DROP TABLE IF EXISTS pacientes CASCADE CONSTRAINTS PURGE;
 
 /* Borrado da táboa "Sanitarios" */
-DROP TABLE sanitarios;
+DROP TABLE IF EXISTS sanitarios CASCADE CONSTRAINTS PURGE;
 
 /* Borrado da táboa "Teléfonos_pacientes" */
-DROP TABLE telefonos_pacientes;
+DROP TABLE IF EXISTS telefonos_pacientes CASCADE CONSTRAINTS PURGE;
 
 /* Borrado da táboa "Teléfonos_sanitarios" */
-DROP TABLE sanitarios;
+DROP TABLE IF EXISTS sanitarios CASCADE CONSTRAINTS PURGE;
---
+++ script_formato.sql
@@ -0,0 +1,29 @@
+/* Script de formato da base de datos do traballo tutelado BD(2019-2020)
+	Opción 1 : Xestión de pandemias
+	Autor: Miguel Blanco Godón 78809752E 
+	Grupo 2.4
+*/
+
+set pages 50
+set feed 1
+
+/* Táboa Pacientes */
+COLUMN dni HEADING 'DNI' FORMAT A9
+COLUMN nome HEADING 'NOME' FORMAT A15
+COLUMN apelidos HEADING 'APELIDOS' FORMAT A30
+COLUMN sexo HEADING 'SEXO' FORMAT A6
+COLUMN data_nacemento HEADING 'DATA DE NACEMENTO' FORMAT A22
+COLUMN enderezo HEADING 'ENDEREZO' FORMAT A60
+COLUMN data_rexistro HEADING 'DATA DE REXISTRO' FORMAT A22
+
+/* Táboa Teléfonos_pacientes */
+COLUMN dni_paciente HEADING 'DNI DO PACIENTE' FORMAT A9
+COLUMN tlf HEADING 'TELÉFONO' FORMAT A9
+
+/* Táboa Sanitarios */
+/* O formato das outras columnas fíxao a táboa de pacientes, pois teñen os mesmos atributos */
+COLUMN categoria HEADING 'CATEGORÍA' FORMAT A12
+
+/* Táboa Teléfonos_sanitarios*/
+/* O formato de tlf xa está definido en Teléfonos_pacientes */
+COLUMN dni_sanitario HEADING 'DNI DO SANITARIO' FORMAT A9
