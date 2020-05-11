cb8f980cc3d42e38646620d1160e4a795cef00a6
meluchoMZ <miguelgodon@outlook.es>
2020-05-11 18:30:50 +0200

    11/05/2020 18:30

--- script_formato.sql
+++ script_formato.sql
@@ -4,26 +4,27 @@
 	Grupo 2.4
 */
 
-set pages 50
-set feed 1
+set pages 50;
+set feed 1;
+set linesize 200;
 
 /* Táboa Pacientes */
-COLUMN dni HEADING 'DNI' FORMAT A9
-COLUMN nome HEADING 'NOME' FORMAT A15
-COLUMN apelidos HEADING 'APELIDOS' FORMAT A30
-COLUMN sexo HEADING 'SEXO' FORMAT A6
-COLUMN data_nacemento HEADING 'DATA DE NACEMENTO' FORMAT A22
-COLUMN enderezo HEADING 'ENDEREZO' FORMAT A60
-COLUMN data_rexistro HEADING 'DATA DE REXISTRO' FORMAT A22
+COLUMN dni HEADING 'DNI' FORMAT A9;
+COLUMN nome HEADING 'NOME' FORMAT A15;
+COLUMN apelidos HEADING 'APELIDOS' FORMAT A30;
+COLUMN sexo HEADING 'SEXO' FORMAT A6;
+COLUMN data_nacemento HEADING 'DATA DE NACEMENTO' FORMAT A22;
+COLUMN enderezo HEADING 'ENDEREZO' FORMAT A40;
+COLUMN data_rexistro HEADING 'DATA DE REXISTRO' FORMAT A22;
 
 /* Táboa Teléfonos_pacientes */
-COLUMN dni_paciente HEADING 'DNI DO PACIENTE' FORMAT A9
-COLUMN tlf HEADING 'TELÉFONO' FORMAT A9
+COLUMN dni_paciente HEADING 'DNI DO PACIENTE' FORMAT A9;
+COLUMN tlf HEADING 'TELÉFONO' FORMAT A9;
 
 /* Táboa Sanitarios */
 /* O formato das outras columnas fíxao a táboa de pacientes, pois teñen os mesmos atributos */
-COLUMN categoria HEADING 'CATEGORÍA' FORMAT A12
+COLUMN categoria HEADING 'CATEGORÍA' FORMAT A12;
 
 /* Táboa Teléfonos_sanitarios*/
 /* O formato de tlf xa está definido en Teléfonos_pacientes */
-COLUMN dni_sanitario HEADING 'DNI DO SANITARIO' FORMAT A9
+COLUMN dni_sanitario HEADING 'DNI DO SANITARIO' FORMAT A9;
--- script_instalacion.sql
+++ script_instalacion.sql
@@ -4,6 +4,8 @@
 	Grupo 2.4
 */
 
+/* CREACIÓN DE TÁBOAS */ 
+
 /* Creación da táboa "Pacientes" */
 CREATE TABLE pacientes (
 	dni VARCHAR(9) CONSTRAINT PK_pacientes PRIMARY KEY CONSTRAINT NN_pacientes_dni NOT NULL,
@@ -20,7 +22,7 @@ CREATE TABLE sanitarios (
 	dni VARCHAR(9) CONSTRAINT PK_sanitarios PRIMARY KEY CONSTRAINT NN_sanitarios_dni NOT NULL,
 	nome VARCHAR(256) CONSTRAINT NN_sanitarios_nome NOT NULL,
 	apelidos VARCHAR(256) CONSTRAINT NN_sanitarios_apelidos NOT NULL,
-	sexo VARCHAR(6) CONSTRAINT NN_sanitarios_apelidos NOT NULL CHECK ((sexo = 'Home') OR (sexo = 'Muller')),
+	sexo VARCHAR(6) CONSTRAINT NN_sanitarios_sexo NOT NULL CHECK ((sexo = 'Home') OR (sexo = 'Muller')),
 	data_nacemento DATE CONSTRAINT NN_sanitarios_data_nacemento NOT NULL,
 	enderezo VARCHAR(512) CONSTRAINT NN_sanitarios_enderezo NOT NULL,
 	categoria VARCHAR(12) CONSTRAINT NN_sanitarios_categoria NOT NULL CHECK ((categoria = 'Medicina') OR (categoria = 'Enfermería') OR (categoria = 'Paramedicina'))
@@ -41,6 +43,7 @@ CREATE TABLE telefonos_sanitarios (
 );
 
 /* INSERCIÓN DE VALORES NAS TÁBOAS */
+
 /* En "Pacientes" */
 INSERT INTO pacientes (dni, nome, apelidos, sexo, data_nacemento, enderezo, data_rexistro) 
 	VALUES ('11111111A', 'Alicia', 'Álvarez Ávila', 'Muller', '01/01/1991', 'R/ Alexandre Bóveda Nº 1, A Coruña', '01/01/2020');
@@ -48,3 +51,25 @@ INSERT INTO pacientes (dni, nome, apelidos, sexo, data_nacemento, enderezo, data
 	VALUES ('22222222B', 'Bernardo', 'Bósforo Bieito', 'Home', '02/02/1992', 'R/ Francis Bacon Nº 2, A Coruña', '02/02/2020');
 INSERT INTO pacientes (dni, nome, apelidos, sexo, data_nacemento, enderezo, data_rexistro)
 	VALUES ('33333333C', 'Carla', 'Castro Cormelle', 'Muller', '03/03/1993', 'R/ Marie Curie Nº 2, A Coruña', '03/03/2020');
+
+/* En "Sanitarios" */
+INSERT INTO sanitarios (dni, nome, apelidos, sexo, data_nacemento, enderezo, categoria)
+	VALUES ('12345678T', 'Margarita', 'Robles Conde', 'Muller', '13/05/1975', 'R/ Alexandre Bóveda Nº 9, A Coruña', 'Enfermería');
+INSERT INTO sanitarios (dni, nome, apelidos, sexo, data_nacemento, enderezo, categoria)
+	VALUES ('98765432Z', 'José Luís', 'López Barreiro', 'Home', '06/11/1962', 'R/ Rosalía de Castro Nº 3, Pontevedra', 'Paramedicina');
+
+/* En "Teléfonos pacientes" */
+INSERT INTO telefonos_pacientes (dni_paciente, tlf)
+	VALUES ('11111111A', 611222333);
+INSERT INTO telefonos_pacientes (dni_paciente, tlf)
+	VALUES ('33333333C', 699888777);
+INSERT INTO telefonos_pacientes (dni_paciente, tlf)
+	VALUES ('33333333C', 688999777);
+
+/* En "Teléfonos sanitarios" */
+INSERT INTO telefonos_sanitarios (dni_sanitario, tlf)
+	VALUES ('12345678T', 620940954);
+INSERT INTO telefonos_sanitarios (dni_sanitario, tlf)
+	VALUES ('12345678T', 765681513);
+INSERT INTO telefonos_sanitarios (dni_sanitario, tlf)
+	VALUES ('98765432Z', 684513518);
